a523543dbad6379889d18e00f58d4d2a423028a6
Jerry Wang <x.jerry.wang@gmail.com>
2022-07-13 22:03:22 +0800

    chore: fix gh release file name

--- x.release.conf.ts
+++ x.release.conf.ts
@@ -1,25 +1,21 @@
 import { defineConfig } from '@0x-jerry/x-release'
 import { readFile, writeFile } from 'fs/promises'
-import toml from '@iarna/toml'
 
 export default defineConfig({
   sequence: [
     //
-    // 'pkg.update.version',
+    'pkg.update.version',
     async (ctx) => {
       const tomlPath = 'src-tauri/Cargo.toml'
       const txt = await readFile(tomlPath, { encoding: 'utf-8' })
 
       const replacedTxt = txt.replace(/version = "[^"]+"/, `version = "${ctx.nextVersion}"`)
 
-      // const conf: any = toml.parse(txt)
-      // conf.package.version = ctx.nextVersion
-
       writeFile(tomlPath, replacedTxt)
-      // console.log(toml.stringify(conf))
     },
-    // 'git.commit',
-    // 'git.tag',
-    // 'git.push',
+    async (ctx) => await ctx.run(`cd src-tauri && cargo check`),
+    'git.commit',
+    'git.tag',
+    'git.push',
   ],
 })
