b4641e4f30a5bd43f190a5de1bf2c94466885bf4
Jerry Wang <x.jerry.wang@gmail.com>
2022-07-13 22:01:24 +0800

    chore: update toml version

--- x.release.conf.ts
+++ x.release.conf.ts
@@ -5,17 +5,21 @@ import toml from '@iarna/toml'
 export default defineConfig({
   sequence: [
     //
-    'pkg.update.version',
+    // 'pkg.update.version',
     async (ctx) => {
       const tomlPath = 'src-tauri/Cargo.toml'
       const txt = await readFile(tomlPath, { encoding: 'utf-8' })
-      const conf: any = toml.parse(txt)
-      conf.package.version = ctx.nextVersion
 
-      writeFile(tomlPath, toml.stringify(conf))
+      const replacedTxt = txt.replace(/version = "[^"]+"/, `version = "${ctx.nextVersion}"`)
+
+      // const conf: any = toml.parse(txt)
+      // conf.package.version = ctx.nextVersion
+
+      writeFile(tomlPath, replacedTxt)
+      // console.log(toml.stringify(conf))
     },
-    'git.commit',
-    'git.tag',
-    'git.push',
+    // 'git.commit',
+    // 'git.tag',
+    // 'git.push',
   ],
 })
