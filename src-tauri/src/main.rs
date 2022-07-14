90c4c03ca30a503225d4545f078c167865dde3ad
Jerry Wang <x.jerry.wang@gmail.com>
2022-07-14 11:04:40 +0800

    perf: check setting when start

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,7 @@
 #[macro_use]
 extern crate serde_derive;
 
+use conf::model::AppConfig;
 use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent::MenuItemClick, SystemTrayMenu};
 
 mod conf;
@@ -19,10 +20,9 @@ fn main() {
     println!("DEV: {}", env::is_dev());
 
     let app_conf = conf::read();
+    start_init(&app_conf);
 
-    conf::save(&app_conf);
-
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit E2Fly").accelerator("CmdOrControl+Q");
     let tray_menu = SystemTrayMenu::new().add_item(quit);
 
     let system_tray = SystemTray::new().with_menu(tray_menu);
@@ -55,4 +55,19 @@ fn main() {
         .menu(tauri::Menu::os_default(&context.package_info().name))
         .run(context)
         .expect("error while running tauri application");
+}
+
+/// 1. autostart v2ray
+/// 2. check system proxy
+fn start_init(conf: &AppConfig) {
+    conf::save(&conf);
+    proxy::set_proxy(&conf);
+
+    if conf.active.enabled {
+        let v2ray = v2fly::get_v2ray_instance();
+        v2ray.run(
+            conf.v2_fly.bin.as_str(),
+            ["-c", conf::get_v2fly_conf_path().to_str().unwrap()],
+        );
+    }
 }
