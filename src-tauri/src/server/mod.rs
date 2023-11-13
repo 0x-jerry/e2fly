0dbeb81ca984adaa80b3ac199279a6141aeda73b
Jerry <x.jerry.wang@gmail.com>
2023-11-14 00:33:01 +0800

    feat: implement all ipc method at http server, wip

--- src-tauri/src/server/mod.rs
+++ src-tauri/src/server/mod.rs
@@ -1,17 +1,77 @@
+use std::fs::File;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
+use crate::{
+    conf::{self, model::AppConfig},
+    env,
+    proxy::set_proxy,
+    utils::tail_from_file,
+    v2fly::get_v2ray_instance,
+};
+
+#[get("/env/is-dev")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    env::is_dev().to_string()
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[post("/config/save")]
+async fn save_config(req_body: web::Json<AppConfig>) -> impl Responder {
+    conf::save(&req_body);
+
+    set_proxy(&req_body);
+    HttpResponse::Ok()
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/config")]
+async fn get_config() -> impl Responder {
+    let config = conf::read();
+
+    HttpResponse::Ok().json(config)
+}
+
+#[get("/v2ray/start")]
+async fn start_v2ray() -> impl Responder {
+    let v2ray = get_v2ray_instance();
+
+    let app_conf = conf::read();
+
+    if let Some(err) = v2ray.start(&app_conf).err() {
+        return err.to_string();
+    }
+
+    "".to_owned()
+}
+
+#[get("/v2ray/stop")]
+async fn stop_v2ray() -> impl Responder {
+    let v2ray = get_v2ray_instance();
+
+    v2ray.stop();
+    HttpResponse::Ok()
+}
+
+#[derive(Deserialize)]
+struct LogPathQuery {
+    p: String,
+}
+
+#[get("/v2ray/log")]
+async fn read_v2ray_log(query: web::Query<LogPathQuery>) -> impl Responder {
+    let f = File::open(query.p.clone());
+
+    let result: Vec<String> = Vec::new();
+
+    if f.is_err() {
+        return HttpResponse::Ok().json(result);
+    }
+
+    let logs = match f {
+        Ok(x) => tail_from_file(&x, 1000),
+        Err(_) => result,
+    };
+
+    HttpResponse::Ok().json(logs)
 }
 
 #[actix_web::main]
@@ -19,8 +79,11 @@ pub async fn start_server() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(save_config)
+            .service(get_config)
+            .service(start_v2ray)
+            .service(stop_v2ray)
+            .service(read_v2ray_log)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
