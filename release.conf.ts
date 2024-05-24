bca579a45e6603230b0ef32198dab9e024f9a3f2
Jerry <x.jerry.wang@gmail.com>
2024-05-24 19:20:47 +0800

    fix: release script

--- release.conf.ts
+++ release.conf.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from '@0x-jerry/x-release'
-import { readJson, writeFile } from 'fs-extra'
+import fs from 'fs-extra'
 
 export default defineConfig({
   publish: false,
   async beforeCommit(ctx) {
     const confPath = 'src-tauri/tauri.conf.json'
-    const conf = await readJson(confPath)
+    const conf = await fs.readJson(confPath)
 
     conf.package.version = ctx.nextVersion.replace(/[a-z]+\./g, '')
 
-    await writeFile(confPath, JSON.stringify(conf, null, 2))
+    await fs.writeFile(confPath, JSON.stringify(conf, null, 2))
   },
 })
