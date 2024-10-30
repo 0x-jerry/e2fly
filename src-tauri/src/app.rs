03dd6f8a9207ac0fa959d64dc60341c5ef6a9fa9
Jerry <x.jerry.wang@gmail.com>
2024-10-30 08:41:10 +0000

    fix: freeeze on macos when save window's state

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -4,7 +4,12 @@ use tauri_plugin_window_state::{AppHandleExt, StateFlags};
 use crate::{conf::AppConfigExt, proxy::set_proxy, v2fly::FlyStateExt};
 
 pub fn before_exit_app<R: Runtime>(app: &AppHandle<R>) {
-    let _ = app.save_window_state(StateFlags::all());
+    let handle = app.clone();
+
+    let _ = app.run_on_main_thread(move || {
+        // this should run on main thread, otherwise, the whole app will freeze on MacOS
+        let _ = handle.save_window_state(StateFlags::all());
+    });
 
     let mut conf = app.app_config();
 
